2a4383fa271be65d47dc53d915eb6cf28067152e
Tsvetomir Shterev <62944181+TsvetomirShterev@users.noreply.github.com>
2022-09-21 16:10:46 +0300

    Added some visual stuff

--- Command-Saver-ui/src/app/app.component.ts
+++ Command-Saver-ui/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Command-Saver-ui';
+export class AppComponent implements OnInit {
+  public title = 'Command-Saver-ui';
+  public collapse = true;
+  public innerWidth = window.innerWidth;
+
+  ngOnInit(): void {
+    this.collapse = this.innerWidth <= 980 ? true : false;
+  }
+
+  toggleCollapse(): void {
+    this.collapse = !this.collapse;
+  }
+
+  onResize(event: any) {
+    this.collapse = event.target.innerWidth <= 980 ? true : false;
+  }
 }
--- Command-Saver-ui/src/app/app.module.ts
+++ Command-Saver-ui/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, FooterComponent, SideMenuComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
